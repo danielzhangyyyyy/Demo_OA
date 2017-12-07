5dcb0e6e3e6106ea8f848dbc700d1bac18283012
zzyang13717 <danielzhang0088@gmail.com>
2017-12-08 11:47:35 +1300

    Revert "new version"
    This reverts commit 5cd73b206ee16595dc4849809e27fabedc942f8e.

--- CZBK.ItcastOA/CZBK.ItcastOA.WebApp/Global.asax.cs
+++ CZBK.ItcastOA/CZBK.ItcastOA.WebApp/Global.asax.cs
@@ -14,6 +14,8 @@ using System.Web.Routing;
 
 namespace CZBK.ItcastOA.WebApp
 {
+    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
+    // 请访问 http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : SpringMvcApplication //System.Web.HttpApplication
     {
